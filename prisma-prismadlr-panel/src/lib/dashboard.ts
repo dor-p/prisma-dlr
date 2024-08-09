46d5dec0ef20a2b55ac05f1e9585e2e0320482a6
Nikunj Sardhara <nsnikunj353@gmail.com>
2024-08-09 22:23:40 +0530

    fix: debug

--- prisma-prismadlr-panel/src/lib/dashboard.ts
+++ prisma-prismadlr-panel/src/lib/dashboard.ts
@@ -1,68 +1,39 @@
 import { runQuery } from './lib';
 export const getDevices = async () => {
-  //   const deviceQuery = `
-  //     SELECT DISTINCT ON (l.device_name)
-  //     l.*,
-  //     c.static_thermal_limit,
-  //     c.conductor_limit_temperature,
-  //     c.is_fiber_monitored,
-  //     c.is_aar,
-  //     c.is_dlr,
-  //     c.circuit_first_span_idx,
-  //     c.circuit_last_span_idx,
-  //     c.conductor_type,
-  //     f.acquisition_timestamp AS forecast_acquisition_timestamp,
-  //     f.valid_for_timestamp AS forecast_valid_for_timestamp,
-  //     f.time_to_overheat AS forecast_time_to_overheat,
-  //     f.thermal_current AS forecast_thermal_current,
-  //     f.conductor_core_temperature AS forecast_conductor_core_temperature,
-  //     f.conductor_skin_temperature AS forecast_conductor_skin_temperature,
-  //     f.conductor_average_temperature AS forecast_conductor_average_temperature,
-  //     f.ambient_temperature AS forecast_ambient_temperature,
-  //     f.wind_speed AS forecast_wind_speed,
-  //     f.wind_direction AS forecast_wind_direction
-  // FROM
-  //     line_dtr_xcel l
-  // LEFT JOIN
-  //     circuits_attributes_xcel c ON l.device_inst_id = c.device_inst_id
-  // LEFT JOIN
-  //     line_dtr_forecast_xcel f ON l.device_inst_id = f.device_inst_id
-  // ORDER BY
-  //     l.device_name, l.valid_for_timestamp DESC
-  // LIMIT 30;
-  //     `;
-
   const deviceQuery = `
-SELECT DISTINCT ON (l.device_name) 
-l.*, 
-c.static_thermal_limit,
-c.conductor_limit_temperature,
-c.is_fiber_monitored,
-c.is_aar,
-c.is_dlr,
-c.circuit_first_span_idx,
-c.circuit_last_span_idx,
-c.conductor_type,
-f.acquisition_timestamp AS forecast_acquisition_timestamp,
-f.valid_for_timestamp AS forecast_valid_for_timestamp,
-f.time_to_overheat AS forecast_time_to_overheat,
-f.thermal_current AS forecast_thermal_current,
-f.conductor_core_temperature AS forecast_conductor_core_temperature,
-f.conductor_skin_temperature AS forecast_conductor_skin_temperature,
-f.conductor_average_temperature AS forecast_conductor_average_temperature,
-f.ambient_temperature AS forecast_ambient_temperature,
-f.wind_speed AS forecast_wind_speed,
-f.wind_direction AS forecast_wind_direction
-FROM 
-line_dtr_xcel l
-LEFT JOIN 
-circuits_attributes_xcel c ON l.device_inst_id = c.device_inst_id
-LEFT JOIN 
-line_dtr_forecast_xcel f ON l.device_inst_id = f.device_inst_id
-LIMIT 30;
-`;
+      SELECT DISTINCT ON (l.device_name)
+      l.*,
+      c.static_thermal_limit,
+      c.conductor_limit_temperature,
+      c.is_fiber_monitored,
+      c.is_aar,
+      c.is_dlr,
+      c.circuit_first_span_idx,
+      c.circuit_last_span_idx,
+      c.conductor_type,
+      f.acquisition_timestamp AS forecast_acquisition_timestamp,
+      f.valid_for_timestamp AS forecast_valid_for_timestamp,
+      f.time_to_overheat AS forecast_time_to_overheat,
+      f.thermal_current AS forecast_thermal_current,
+      f.conductor_core_temperature AS forecast_conductor_core_temperature,
+      f.conductor_skin_temperature AS forecast_conductor_skin_temperature,
+      f.conductor_average_temperature AS forecast_conductor_average_temperature,
+      f.ambient_temperature AS forecast_ambient_temperature,
+      f.wind_speed AS forecast_wind_speed,
+      f.wind_direction AS forecast_wind_direction
+  FROM
+      line_dtr_xcel l
+  LEFT JOIN
+      circuits_attributes_xcel c ON l.device_inst_id = c.device_inst_id
+  LEFT JOIN
+      line_dtr_forecast_xcel f ON l.device_inst_id = f.device_inst_id
+  ORDER BY
+      l.device_name, l.valid_for_timestamp DESC
+  LIMIT 30;
+      `;
 
   const deviceResults = await runQuery(deviceQuery);
+  console.log('deviceResults', deviceResults);
   return deviceResults;
 };
 
